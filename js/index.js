8fa500959e944b8252a515abcf98b3af2ea8b185
kl1302uc <125315863+kl1302uc@users.noreply.github.com>
2024-02-28 18:36:09 +0800

    功能实验

--- js/index.js
+++ js/index.js
@@ -1,5 +1,39 @@
 import '../components/pipe.js';
-const pipe=document.querySelectorAll('swh-pipe');
-pipe.forEach((el,index)=>{
+import {TimedAjaxRequest} from './request.js';
+const shangbu=document.querySelectorAll('.shangbu');
+const xiabu=document.querySelectorAll('.xiabu');
+const zhuguandao=document.querySelector(".zhuguandao");
+const shangbubeng=document.querySelector(".shangbubeng");
+const xiabubeng=document.querySelector(".xiabubeng");
+const shuixiangDetail=document.querySelector('.shuixiang>.detail');
+const shuiwei=document.querySelector('.shuixiang>.shuiweiWrap>.shuiwei');
+
+/*控制上循环泵开关*/
+function shangbukaiguan(valueBool){
+    shangbubeng.style.animationPlayState=valueBool?"running":"paused";
+    shangbu.forEach((el,index)=>{
+        el.start=valueBool?"true":"false";
+    });  
+}
+/*控制下循环泵开关*/
+function xiabukaiguan(valueBool){
+    xiabubeng.style.animationPlayState=valueBool?"running":"paused";
+    xiabu.forEach((el,index)=>{
+        el.start=valueBool?"true":"false";
+    })
+}
+
+/*获取太阳能数据*/
   
-})
+
+const dataFetcher = new TimedAjaxRequest('http://center.zhuineng.com:50020/api-t/translate?type=0&uid=329c7b6dee7c46f095a4fb7d93c79089&pid=bb85a5219375426fb805ebacef87d65b', 5000,(data)=>{
+    shangbukaiguan(data.KP1S);
+
+    xiabukaiguan(data.KP2S);
+  
+    shuixiangDetail.innerText=`温度:${data.T0}℃
+    水位:${data.W1}%`;
+    shuiwei.style.height=data.W1+'%';
+
+}); // 每 5 秒获取一次数据
+dataFetcher.start();
---
+++ js/request.js
@@ -0,0 +1,72 @@
+// dataFetcher.js
+
+export class DataFetcher {
+  constructor(url, interval) {
+    this.url = url;
+    this.interval = interval;
+  }
+
+  fetchData() {
+    return fetch(this.url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
+  startFetching(callback) {
+    this.fetchData().then(data => {
+      callback(data.data);
+    });
+
+    this.intervalId = setInterval(() => {
+      this.fetchData().then(data => {
+        callback(data.data);
+      });
+    }, this.interval);
+  }
+
+  stopFetching() {
+    clearInterval(this.intervalId);
+  }
+}
+
+export class TimedAjaxRequest {
+  constructor(url, interval, callback) {
+      this.url = url;
+      this.interval = interval;
+      this.timer = null;
+      this.callback = callback;
+  }
+
+  start() {
+      this.timer = setInterval(() => {
+          this.makeRequest();
+      }, this.interval);
+  }
+
+  stop() {
+      clearInterval(this.timer);
+  }
+
+  makeRequest() {
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', this.url, true);
+      xhr.onreadystatechange = () => {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+              // 调用回调函数处理返回的数据
+              this.callback(xhr.responseText.data);
+              console.log(xhr.responseText.data);
+          }else{
+             console.warn("数据错误",xhr.responseText);
+          }
+      };
+      xhr.send();
+  }
+}
+
