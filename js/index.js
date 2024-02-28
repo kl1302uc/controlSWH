d1da2b0d27014a063dfab0a5c9031e4d7af20e93
kl1302uc <125315863+kl1302uc@users.noreply.github.com>
2024-02-28 19:27:18 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import '../components/pipe.js';
-import {TimedAjaxRequest} from './request.js';
+import {dataFetcher } from './request.js';
 const shangbu=document.querySelectorAll('.shangbu');
 const xiabu=document.querySelectorAll('.xiabu');
 const zhuguandao=document.querySelector(".zhuguandao");
@@ -26,14 +26,14 @@ function xiabukaiguan(valueBool){
 /*获取太阳能数据*/
   
 
-const dataFetcher = new TimedAjaxRequest('http://center.zhuineng.com:50020/api-t/translate?type=0&uid=329c7b6dee7c46f095a4fb7d93c79089&pid=bb85a5219375426fb805ebacef87d65b', 5000,(data)=>{
-    shangbukaiguan(data.KP1S);
+const dataFetcher = new DataFetcher('http://center.zhuineng.com:50020/api-t/translate?type=0&uid=329c7b6dee7c46f095a4fb7d93c79089&pid=bb85a5219375426fb805ebacef87d65b', 5000); // 每 5 秒获取一次数据
+dataFetcher.startFetching((data)=>{
+  shangbukaiguan(data.KP1S);
 
-    xiabukaiguan(data.KP2S);
-  
-    shuixiangDetail.innerText=`温度:${data.T0}℃
-    水位:${data.W1}%`;
-    shuiwei.style.height=data.W1+'%';
+  xiabukaiguan(data.KP2S);
 
-}); // 每 5 秒获取一次数据
-dataFetcher.start();
+  shuixiangDetail.innerText=`温度:${data.T0}℃
+  水位:${data.W1}%`;
+  shuiwei.style.height=data.W1+'%';
+
+});
